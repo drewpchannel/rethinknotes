f206329ecb01f3ee4c7067974b0c765eb1bd8cd2
drew <drewpcchannel@gmail.com>
2018-09-02 13:28:28 -0600

    reverting

--- index.js
+++ index.js
@@ -2,75 +2,58 @@ const r = require('rethinkdb');
 
 let connection = null;
 
-function connect() {
-  if connection (!= null) {
+function connect(siteName, price) {
+  let connection = null;
+  (() => {
     r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
-      if (err) throw err;
+      console.log('defining conn as ' + conn)
       connection = conn;
-      return connection;
+    }).then(value => {
+      console.log('runnign check')
+      checkDuplicates(siteName, price, connection);      
     })
-  } else {
-    return connection;
-  }
+  })()
 }
 
-function checkDuplicates (siteName, price) {
-  r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
-    if (err) throw err;
-    connection = conn;
-  })
-  .then(value => {
-    r.table('ttable').filter(r.row('name').eq(siteName)).
-      run(connection, function(err, cursor) {
-          if (err) throw err;
-          cursor.toArray(function(err, result) {
-              if (err) throw err;
-              let parsedResults = JSON.stringify(result, null, 2);
-              if (parsedResults.length < 10) {
-                addNewWebsite(siteName, price);
-              } else {
-                updatePrices(siteName, price);
-              }
-          });
-      });
-  })
-}
-
-function addNewWebsite (name, price) {
-  if (typeof name === 'string') {
-    r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
-      if (err) throw err;
-      connection = conn;
-    })
-    .then(value =>{
-      r.table('ttable').insert([
-      { 
-        name: name, pc: price
-      }
-      ]).run(connection, function(err, result) {
+function checkDuplicates (siteName, price, connection) {
+  r.table('ttable').filter(r.row('name').eq(siteName)).
+    run(connection, function(err, cursor) {
         if (err) throw err;
-        console.log(JSON.stringify(result, null, 2));
-      })
-    })
-  }
+        cursor.toArray(function(err, result) {
+            if (err) throw err;
+            let parsedResults = JSON.stringify(result, null, 2);
+            if (parsedResults.length < 10) {
+              addNewWebsite(siteName, price);
+            } else {
+              updatePrices(siteName, price);
+            }
+        });
+    });
 }
 
-function updatePrices (siteName, price) {
-  r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
-    if (err) throw err;
-    connection = conn;
-  })
-  .then(value => {
-    r.table('ttable').filter({name: siteName}).update({
-      pc: price
-    }).run(connection, function(err, result) {
+function addNewWebsite (name, price, connection) {
+  if (typeof name === 'string') {
+    r.table('ttable').insert([
+    { 
+      name: name, pc: price
+    }
+    ]).run(connection, function(err, result) {
       if (err) throw err;
       console.log(JSON.stringify(result, null, 2));
     })
+  }
+}
+
+function updatePrices (siteName, price, connection) {
+  r.table('ttable').filter({name: siteName}).update({
+    pc: price
+  }).run(connection, function(err, result) {
+    if (err) throw err;
+    console.log(JSON.stringify(result, null, 2));
   })
 }
 
-checkDuplicates("Madden Stories", 205.00);
+connect("Madden Stories", 205.00);
 
 //creating a new table
 /*
