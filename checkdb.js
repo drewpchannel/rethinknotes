241a01047fa7b3e303b96f90649cdc28ee1769b4
drew <drewpcchannel@gmail.com>
2018-09-02 14:17:38 -0600

    changed to correct db

--- checkdb.js
+++ checkdb.js
@@ -1,10 +1,11 @@
-const r = require('rethinkdb');
+var r = require('rethinkdb');
 
-r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+r.connect( {host: 'localhost', port: 28015, db: 'CoinPrices'}, function(err, conn) {
   if (err) throw err;
   connection = conn;
-}).then(value => {
-  r.table('ttable').run(connection, (err, cursor) => {
+})
+.then(value => {
+  r.table('websites').run(connection, (err, cursor) => {
     if (err) throw err;
     cursor.toArray((err, result) => {
       if (err) throw err;
--- index.js
+++ index.js
@@ -2,58 +2,66 @@ const r = require('rethinkdb');
 
 let connection = null;
 
-function connect(siteName, price) {
-  let connection = null;
-  (() => {
-    r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
-      console.log('defining conn as ' + conn)
+function checkDuplicates (siteName, pricePs, pricePc, priceXb) {
+  r.connect( {host: 'localhost', port: 28015, db: 'CoinPrices'}, function(err, conn) {
+    connection = conn;
+  })
+  .then (value =>{
+    r.table('websites').filter(r.row('name').eq(siteName)).
+      run(connection, function(err, cursor) {
+          if (err) throw err;
+          cursor.toArray(function(err, result) {
+              if (err) throw err;
+              let parsedResults = JSON.stringify(result, null, 2);
+              if (parsedResults.length < 10) {
+                addNewWebsite(siteName, pricePs, pricePc, priceXb);
+              } else {
+                updatePrices(siteName, pricePs, pricePc, priceXb);
+              }
+          });
+      });
+  })
+}
+
+function addNewWebsite (siteName, pricePs, pricePc, priceXb) {
+  if (typeof siteName === 'string') {
+    r.connect( {host: 'localhost', port: 28015, db: 'CoinPrices'}, function(err, conn) {
       connection = conn;
-    }).then(value => {
-      console.log('runnign check')
-      checkDuplicates(siteName, price, connection);      
     })
-  })()
-}
-
-function checkDuplicates (siteName, price, connection) {
-  r.table('ttable').filter(r.row('name').eq(siteName)).
-    run(connection, function(err, cursor) {
+    .then(value => {
+      r.table('websites').insert([
+      { 
+        name: siteName, 
+        ps: pricePs,
+        pc: pricePc, 
+        xb: priceXb
+      }
+      ]).run(connection, function(err, result) {
         if (err) throw err;
-        cursor.toArray(function(err, result) {
-            if (err) throw err;
-            let parsedResults = JSON.stringify(result, null, 2);
-            if (parsedResults.length < 10) {
-              addNewWebsite(siteName, price);
-            } else {
-              updatePrices(siteName, price);
-            }
-        });
-    });
-}
-
-function addNewWebsite (name, price, connection) {
-  if (typeof name === 'string') {
-    r.table('ttable').insert([
-    { 
-      name: name, pc: price
-    }
-    ]).run(connection, function(err, result) {
-      if (err) throw err;
-      console.log(JSON.stringify(result, null, 2));
+        console.log(JSON.stringify(result, null, 2));
+      })
     })
   }
 }
 
-function updatePrices (siteName, price, connection) {
-  r.table('ttable').filter({name: siteName}).update({
-    pc: price
-  }).run(connection, function(err, result) {
-    if (err) throw err;
-    console.log(JSON.stringify(result, null, 2));
+function updatePrices (siteName, pricePs, pricePc, priceXb) {
+  r.connect( {host: 'localhost', port: 28015, db: 'CoinPrices'}, function(err, conn) {
+    connection = conn;
+  })
+  .then(value => {
+    r.table('websites').filter({name: siteName}).update({
+      name: siteName, 
+      ps: pricePs,
+      pc: pricePc, 
+      xb: priceXb
+    }).run(connection, function(err, result) {
+      if (err) throw err;
+      console.log(JSON.stringify(result, null, 2));
+    })
   })
 }
 
-connect("Madden Stories", 205.00);
+checkDuplicates('The MUT Market', '21.00', '34.00', '21.00');
 
 //creating a new table
 /*
@@ -65,7 +73,6 @@ connect("Madden Stories", 205.00);
 });
 */
 
-//inserting data into that table (incorrect way)
 /*
   r.table('authors').insert([
   { websites: [
@@ -92,7 +99,7 @@ connect("Madden Stories", 205.00);
   })
 */
 
-//find specific value for every name column
+//search for a specific item in table
 /*
   r.table('authors').filter(r.row('websites').column('name').eq("Madden Coin Store")).
       run(connection, function(err, cursor) {
@@ -119,7 +126,7 @@ connect("Madden Stories", 205.00);
   })
 */
 
-//find everything with madden value
+//search for specific value
 /*
   r.table('authors2').filter(r.row('name').eq("Madden")).
     run(connection, function(err, cursor) {
@@ -131,14 +138,15 @@ connect("Madden Stories", 205.00);
     });
 */
 
+//connect with specific DB
 /*
-  r.connect( {host: 'localhost', port: 28015}, function(err, conn) {
+  r.connect( {host: 'localhost', port: 28015, db: 'CoinPrices'}, function(err, conn) {
     if (err) throw err;
     connection = conn;
   })
 */
 
-//update data based on the table authors2, column name Madden
+//update specefic value
 /*
   r.table('authors2').filter({name: "Madden"}).update({
     pc: 99.00
